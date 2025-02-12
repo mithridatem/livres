0229da91dc0be43597e1a89a862cab2d9ad59f35
mithridatem <mathieumithridate@adrar-formation.com>
2025-02-12 10:27:52 +0100

    config bdd

---
+++ src/Env.java
@@ -0,0 +1,5 @@
+public class Env {
+    public final String DB_URL = "jdbc:mysql://localhost/livres?serverTimezone=UTC";
+    public final String DB_LOGIN = "root";
+    public final String DB_PASSWORD = "";
+}
